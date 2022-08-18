9d83b1afa37d00db90160da690de270fe2886beb
Djenaba <djenabacooper@gmail.com>
2022-08-18 00:00:14 -0500

    Added lists

---
+++ Day trip generator.py
@@ -0,0 +1,6 @@
+vacay_destination = ['Chicago', 'New York', 'Miami']
+chi_restaurants = ['Giordanos', 'Leonas', 'The Purple Pig']
+ny_restaurants = ['Gramercy Tavern', 'La Grande Boucherie', 'Raouls']
+mi_restaurants = ['Red Rooster', 'Cvi.che 105', 'Mignonette Downtown']
+transportation = ['Car', 'Train', 'Plane']
+entertainment = ['A play', 'A concert', 'Netflix and chill']
